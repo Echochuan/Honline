76c7b58080886f7b5e11ab110ebd2c19e37ac734
Echochuan <912297261@qq.com>
2021-12-15 01:14:07 +0800

    :sparkles: 设置默认路由

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/login/index";
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
