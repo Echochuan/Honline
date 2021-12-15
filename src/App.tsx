79f98ac014eddc43e4988b6b4438bc2144064ca9
Echochuan <912297261@qq.com>
2021-12-16 00:05:03 +0800

    :sparkles: 编写注册页面样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login/index";
+import Register from "./pages/register/index";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register/>} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
---
+++ src/components/registerBox/index.tsx
@@ -0,0 +1,59 @@
+import { Form, Input, Button } from "antd";
+import './index.css';
+import { UserOutlined , KeyOutlined } from '@ant-design/icons';
+
+const RegisterBox = () => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div className="registerBox">
+      <div className="registerBox1">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 24 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input size="large" placeholder="请输入账号" prefix={<UserOutlined />} />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password size="large" placeholder="请输入密码" prefix={<KeyOutlined />}/>
+          </Form.Item>
+
+          <Form.Item
+            name="password2"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password size="large" placeholder="请确认你的密码" prefix={<KeyOutlined />}/>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 24 }}>
+            <Button size="large" type="primary" htmlType="submit">
+              注册
+            </Button>
+          </Form.Item>
+
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterBox;
