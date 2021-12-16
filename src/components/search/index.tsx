a21b656b61b2597d2d4cf0479b67dd0c1018e0f9
Echochuan <912297261@qq.com>
2021-12-16 18:45:09 +0800

    :sparkles: 调整搜索框样式

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,7 +1,12 @@
 import { RightCircleTwoTone } from "@ant-design/icons";
+import { message } from "antd";
 import "./index.css";
 
 const Search = () => {
+  const onClick = () => {
+    message.error("暂未开放");
+  }
+
   return (
     <div className="search bar">
       <form>
@@ -10,12 +15,13 @@ const Search = () => {
           type="text"
           placeholder="请输入您要搜索的内容......"
         />
-        <button type="submit">
+
+      </form>
+      <button onClick={ onClick }>
           <div className="icon-logo">
             <RightCircleTwoTone  twoToneColor="f9f0da" style={{fontSize: '26px'}}/>
           </div>
         </button>
-      </form>
     </div>
   );
 };
