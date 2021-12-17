baae5aa43ccd6b2b2af8b2919d6c14f0e9612374
Echochuan <912297261@qq.com>
2021-12-17 08:41:51 +0800

    :sparkles: 完成对商品列表数据的一次性渲染

--- src/components/goodsList/index.tsx
+++ src/components/goodsList/index.tsx
@@ -1,36 +1,42 @@
-import goods from '../../mock/goods.json'
+import goods from "../../mock/goods.json";
 
-interface dataList  {
-    img: string;
-    context: string;
-    price: string;
+interface dataList {
+  img: string;
+  context: string;
+  price: string;
 }
 
-const List = (goodsList:dataList[]) => {
-    return (
-         <div>
-             {/* 当 goods 不是对象数组的时候能渲染 */}
-         {goodsList.map((goods) => {
-             <img src={goods.img}></img>
-        })} 
+const List = (goodsList: dataList[]) => {
+  let stageList: any = [];
+  //eslint-disable-next-line
+  {
+    goodsList.map((item, i) => {
+      stageList.push(
+        <div key={i}>
+          <div className="imgBox">
+            <img src={item.img} alt="" />
+          </div>
+          <div className="contextBox" >
+            {item.context}
+          </div>
         </div>
-    )
-}
+      );
+      return 0;
+    });
+  }
+  return <div>{stageList}</div>;
+};
 
 const GoodsList = () => {
-    const goodsList:dataList[] = goods.goodsList;
-    console.log(goodsList)
-    const imgList: string[] = [];
-    const contextList: string[] = [];
-    for(const i in goodsList) {
-         imgList.push(goodsList[i].img)
-         contextList.push(goodsList[i].context)
-    }
-    return(
-        <div>
-            {List(goodsList)}
-        </div>
-    )
-}
+  const goodsList: dataList[] = goods.goodsList;
+  console.log(goodsList);
+  const imgList: string[] = [];
+  const contextList: string[] = [];
+  for (const i in goodsList) {
+    imgList.push(goodsList[i].img);
+    contextList.push(goodsList[i].context);
+  }
+  return <div>{List(goodsList)}</div>;
+};
 
 export default GoodsList;
