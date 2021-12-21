8f7fa487bc1f96665e373571645c573c6d5e620f
Echochuan <912297261@qq.com>
2021-12-21 21:15:52 +0800

    :lipstick: 更新购物车样式

--- src/components/shoppingList/ItemCard.tsx
+++ src/components/shoppingList/ItemCard.tsx
@@ -1,54 +1,57 @@
-import React from "react"
-import { CartItem } from "./"
-import { OnCheckedChange } from './use-check'
-import { Typography } from "antd"
-import './index.css'
+import React from "react";
+import { CartItem } from "./";
+import { OnCheckedChange } from "./use-check";
+import { Typography } from "antd";
+import "./index.css";
 
 interface Props {
-  item: CartItem
-  checked: boolean
-  onCheckedChange: OnCheckedChange<CartItem>
+  item: CartItem;
+  checked: boolean;
+  onCheckedChange: OnCheckedChange<CartItem>;
 }
 
 // memo优化策略
 function areEqual(prevProps: Props, nextProps: Props) {
-  return (
-    prevProps.checked === nextProps.checked
-  )
+  return prevProps.checked === nextProps.checked;
 }
 
 const ItemCard = React.memo((props: Props) => {
-  console.log('cart item rerender')
-  const { item, checked, onCheckedChange } = props
-  const { goodsTitle, goodsPrice, goodsSrc } = item
+  console.log("cart item rerender");
+  const { item, checked, onCheckedChange } = props;
+  const { goodsTitle, goodsPrice, goodsSrc, goodsSubtitle } = item;
 
   const onWrapCheckedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { checked } = e.target
-    onCheckedChange(item, checked)
-  }
-  
+    const { checked } = e.target;
+    onCheckedChange(item, checked);
+  };
+
   return (
     <div className="each-shopping-good">
       <div className="checkbox-wrap">
         <input
-            className="checkBox"
+          className="checkBox"
           type="checkBox"
           checked={checked}
           onChange={onWrapCheckedChange}
         />
       </div>
-      <p className="item-info">
-      <div>
-            <img src={goodsSrc} style={{ width:"100px",height:"100px" }} alt=""/>
-          </div>
-          <div className="shopping-content">
-            {goodsTitle}
-          </div>
-        <Typography.Text mark className="shopping-price">${goodsPrice}</Typography.Text>
+      <p>
+        <div>
+          <img
+          className="shopping-img"
+            src={goodsSrc}
+            style={{ width: "100px", height: "100px" }}
+            alt=""
+          />
+        </div>
+        <div className="shopping-title">{goodsTitle}</div>
+        <div className="shopping-content">{goodsSubtitle}</div>
+        <Typography.Text mark className="shopping-price">
+          ${goodsPrice}
+        </Typography.Text>
       </p>
     </div>
-  )
-}, areEqual)
+  );
+}, areEqual);
 
-
-export default ItemCard
+export default ItemCard;
