9dad982c0beeb64e0bc72a794ce249e518de48e8
Echochuan <912297261@qq.com>
2021-12-22 00:10:45 +0800

    :sparkles: 添加删除购物车内商品的功能

--- src/components/shoppingList/ItemCard.tsx
+++ src/components/shoppingList/ItemCard.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { CartItem } from "./";
 import { OnCheckedChange } from "./use-check";
-import { Typography } from "antd";
+import { Modal, Typography } from "antd";
 import "./index.css";
+import { Button } from "antd/lib/radio";
+import store from "../../redux/store";
 
 interface Props {
   item: CartItem;
@@ -16,17 +18,40 @@ function areEqual(prevProps: Props, nextProps: Props) {
 }
 
 const ItemCard = React.memo((props: Props) => {
+  const [visible, setVisible] = useState(false);
+
   console.log("cart item rerender");
   const { item, checked, onCheckedChange } = props;
-  const { goodsTitle, goodsPrice, goodsSrc, goodsSubtitle } = item;
+  const { id, goodsTitle, goodsPrice, goodsSrc, goodsSubtitle } = item;
 
   const onWrapCheckedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { checked } = e.target;
     onCheckedChange(item, checked);
   };
 
+  const deleteGood = () => {
+    //获取商品的 id
+    console.log(id);
+    //获取用户的 id
+    const userId = store.getState().name;
+    //将数据发给后端，后端返回正确的状态码之后刷新页面
+    window.location.href = "/shoppingCar";
+  };
+
   return (
     <div className="each-shopping-good">
+      <Modal
+        visible={visible}
+        title="确认删除吗？"
+        okText="确认"
+        cancelText="再想想"
+        onCancel={() => {
+          setVisible(false);
+        }}
+        onOk={() => {
+          deleteGood();
+        }}
+      ></Modal>
       <div className="checkbox-wrap">
         <input
           className="checkBox"
@@ -38,7 +63,7 @@ const ItemCard = React.memo((props: Props) => {
       <p>
         <div>
           <img
-          className="shopping-img"
+            className="shopping-img"
             src={goodsSrc}
             style={{ width: "100px", height: "100px" }}
             alt=""
@@ -49,6 +74,16 @@ const ItemCard = React.memo((props: Props) => {
         <Typography.Text mark className="shopping-price">
           ${goodsPrice}
         </Typography.Text>
+        <div className="btn-delete">
+          <Button
+            type="primary"
+            onClick={() => {
+              setVisible(true);
+            }}
+          >
+            删除
+          </Button>
+        </div>
       </p>
     </div>
   );
