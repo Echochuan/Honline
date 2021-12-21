567609dba7f1b350c17f08a0496f8392b16d2ce3
Echochuan <912297261@qq.com>
2021-12-21 23:47:09 +0800

    :sparkles: 优化注册页面的跳转逻辑

--- src/components/registerBox/index.tsx
+++ src/components/registerBox/index.tsx
@@ -2,28 +2,26 @@ import { Form, Input, Button, message } from "antd";
 import "./index.css";
 import { UserOutlined, KeyOutlined } from "@ant-design/icons";
 import store from "../../redux/store";
+import { getName } from "../../redux/action";
 
 const RegisterBox = () => {
 
   const onFinish = (values: any) => {
-    console.log("subscribe", store.getState());
-    const name = store.getState().name;
 
     console.log("Success:", values);
     if (values.password !== values.password2) {
       message.error("请输入两次一致的密码");
-
-      //测试取出 store
-    console.log("next :")
-    console.log(name);
-
-    store.subscribe(() => {
-        console.log("subscribe", store.getState());
-      });
     } else {
       //在这里调用接口，把账户和密码发送给后端存储
+      //后端返回该用户的 id 将它存入 store 中
+      const action = getName("101"); /**将这里的 101 更换为获取的用户 id */
+      store.dispatch(action);
+      console.log(store.getState())
+      store.subscribe(() => {
+        console.log("subscribe", store.getState());
+      });
       //然后跳转到主页
-      window.location.href = "/login";
+      window.location.href = "/init";
     }
   };
 
