eb79ce045f717115ab1fcc810fbe38b0fb5aa5da
Echochuan <912297261@qq.com>
2021-12-22 11:33:01 +0800

    :sparkles: 更新开店的气泡确认框

--- src/pages/Init/index.tsx
+++ src/pages/Init/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import { Layout, Button, Row, Col } from "antd";
+import { Layout, Button, Row, Col, Popconfirm, message } from "antd";
 import { ShoppingCartOutlined, SmileOutlined } from "@ant-design/icons";
 
 import Search from "../../components/search/index";
@@ -26,34 +26,54 @@ const Init = () => {
     console.log("subscribe", store.getState());
   });
 
-  const topMenu = () =>{
-    console.log("111")
+  const openStore = () => {
+    console.log("111");
+  };
+
+  const confirm = () => {
+    message.success('注册成功');
+  }
+  
+  const cancel = () => {
+    message.error('下次一定');
+  }
+
+  const topMenu = () => {
+    console.log("111");
     //向后端发送请求，询问该用户是不是已经有商店
     const haveStore = false;
     // const donthave = false;
     if (haveStore) {
-      return(
-        <div className="shortcut">111</div>
-      )
-    }
-    else {
-      return(
+      return <div className="shortcut">111</div>;
+    } else {
+      return (
         <div className="shortcut">
-          <a href="#" className="btn-store">
-            我要开店
-          </a>
+          {" "}
+          <Popconfirm
+            title="Are you sure to delete this task?"
+            onConfirm={confirm}
+            onCancel={cancel}
+            okText="Yes"
+            cancelText="No"
+          >
+            <a href="#" className="btn-store" onClick={openStore}>
+              我要开店
+            </a>
+          </Popconfirm>
         </div>
-      )
+      );
     }
-  }
+  };
 
   return (
     <div>
       {/* <button>111</button> */}
-      { topMenu() }
+      {topMenu()}
       <Layout style={{ height: "100vh", display: "block" }}>
         <Header className="header-init">
-          <a href="/init"><div className="logo-init"></div></a>
+          <a href="/init">
+            <div className="logo-init"></div>
+          </a>
           <Search />
           <Button
             type="primary"
@@ -71,7 +91,9 @@ const Init = () => {
             <div className="floor">
               <Row>
                 <Col span={8}></Col>
-                <Col span={8} className="floorhd"><SmileOutlined /> <b>为您推荐</b></Col>
+                <Col span={8} className="floorhd">
+                  <SmileOutlined /> <b>为您推荐</b>
+                </Col>
                 <Col span={8}></Col>
               </Row>
             </div>
