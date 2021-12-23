0a6356a7411c0637f984ca2e5dd987e84a86fc41
Echochuan <912297261@qq.com>
2021-12-23 14:59:09 +0800

    :sparkles: 初始化系统维护页面

---
+++ src/pages/inSafeguard/index.tsx
@@ -0,0 +1,24 @@
+import { Layout } from "antd";
+import "./index.css"
+
+const { Header, Footer, Content } = Layout;
+
+const InSafeguard = () => {
+  return (
+    <div>
+      <Layout style={{ height: "100vh" }}>
+        <Header>
+            <div className="logo" >
+            </div>
+        </Header>
+        <Layout>
+          <Content>
+          </Content>
+        </Layout>
+        <Footer>Copyright © 2021  Created by 524 & 525</Footer>
+      </Layout>
+    </div>
+  );
+};
+
+export default InSafeguard;
